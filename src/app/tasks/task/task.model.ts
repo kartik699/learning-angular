6e8d08c9c950999ba72233a89fe6fcc8150d3c49
Kartik Gandhi <kartik.gandhi@tothenew.com>
2025-05-21 16:31:36 +0530

    build task component

---
+++ src/app/tasks/task/task.model.ts
@@ -0,0 +1,3 @@
+import { dummyTasks } from '../dummy-tasks';
+
+export type Task = (typeof dummyTasks)[0];
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,21 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { dummyTasks } from './dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input() selectedUserName?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) selectedUserName!: string;
+
+  tasks = dummyTasks;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
