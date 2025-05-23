11aefb8a45576ff886b57c0d745f5c480bad1555
Kartik Gandhi <gandhikartik73@gmail.com>
2025-05-24 00:21:14 +0530

    use delete service in task component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -23,10 +23,6 @@ export class TasksComponent {
     return this.tasksService.getUserTasks(this.userId);
   }
 
-  onCompleteTask(taskId: string) {
-    this.tasksService.removeTask(taskId);
-  }
-
   onStartAddTask() {
     this.isAddingTask = true;
   }
