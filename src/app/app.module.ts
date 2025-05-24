84fad380fe0fadb0f2f6065aa635d9ebc2353227
Kartik Gandhi <gandhikartik73@gmail.com>
2025-05-24 20:21:08 +0530

    change to module based approach

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from './shared/shared.module';
+import { TasksModule } from './tasks/tasks.module';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  imports: [BrowserModule, SharedModule, TasksModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
